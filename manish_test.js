402420f528ed0702a9dd154cadb1f19379d03142
Orendra Singh <orendrasingh@gmail.com>
2022-10-31 12:08:53 +0530

    Changed the menu card design how it needs to look llike

--- manish_test.js
+++ manish_test.js
@@ -1,4 +1,4 @@
-console.log("First commit for hactober!");
+console.log("First commit for hactober 2022!");
 console.log("Second commit for hactober!");
 console.log("Third commit for hactober!");
 console.log("Forth commit for hactober!");
